61e6dd02a6421a8e812fea7809b943562c6d9796
Rodrigo Martín de Lamo <rmartin@devcenter.es>
2018-10-24 16:54:08 +0200

    Added html-webpack-plugin

--- dev/index.js
+++ dev/index.js
@@ -2,10 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 Vue.config.productionTip = false
 
-window.onload = function () {
-console.log(document.getElementById('app'));
-
-    new Vue({
-        render: h => h(App)
-    }).$mount('#app')
-}
+new Vue({
+    render: h => h(App)
+}).$mount('#app')
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './dev/index.js',
@@ -29,6 +30,7 @@ module.exports = {
         ]
     },
     plugins: [
-        new VueLoaderPlugin()
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({ template: './dev/index.html' }),
     ]
 }
