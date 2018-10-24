e373dac740476b08281a8e2271b076bb479ab2ef
Rodrigo Martín de Lamo <rmartin@devcenter.es>
2018-10-24 15:49:55 +0200

    Added dev folder to load the component and develop it

---
+++ dev/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <LoginButton :client_id="client_id" :onSuccess="onSignIn" :onFailure="onFailure"></LoginButton>
+</template>
+
+<script>
+import { LoginButton } from '../src/index';
+const CLIENT_ID = "672275288589-gdg4j010jalhp5n6gh45333dhuq69liv.apps.googleusercontent.com";
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            client_id: CLIENT_ID,
+        }
+    },
+    components: {
+        LoginButton
+    },
+    methods: {
+        onSignIn(googleUser) {
+            console.log(googleUser);
+        },
+        onFailure(err) {
+            console.log(err)
+        }
+    }
+}
+</script>
+
+<style>
+</style>
