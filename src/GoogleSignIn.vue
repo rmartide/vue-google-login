2e9f798ac1c995712e3f11f0c2788b72148a203c
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2018-10-27 21:38:47 +0200

    Removed console.logs

--- src/GoogleSignIn.vue
+++ src/GoogleSignIn.vue
@@ -43,11 +43,7 @@ export default {
         }
     },
     mounted() {
-        GoogleAuth.load(this.client_id).then(auth2 => {
-            console.log(auth2)
-        }).catch(err => {
-            console.log(err);
-        });
+        GoogleAuth.load(this.client_id);
     }
 }
 </script>
